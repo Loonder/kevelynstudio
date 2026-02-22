8eaa583093cb59521c00a046fca319d58dea95a5
Loonder <loonderpno@gmail.com>
2026-02-22 03:11:45 -0300

    fix(types): add declaration for react-big-calendar to fix build

---
+++ src/types/react-big-calendar.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-big-calendar';
+declare module 'react-big-calendar/lib/addons/dragAndDrop';
+declare module 'react-big-calendar/lib/css/react-big-calendar.css';
+declare module 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
